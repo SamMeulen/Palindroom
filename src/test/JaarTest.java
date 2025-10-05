2d61468749e075da90ce8aea0d43e22cdb63e955
sam <depapa_33@yahoo.com>
2025-10-06 00:19:29 +0200

    refactor test unit

--- src/test/JaarTest.java
+++ src/test/JaarTest.java
@@ -3,27 +3,27 @@ package test;
 import main.Jaar;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class JaarTest {
     @Test
     public void eenJaarDeelbaarDoor400IsEenSchrikkeljaar() {
         Jaar jaar = new Jaar(2000);
-        assertEquals(true, jaar.isSchrikkeljaar());
+        assertTrue(jaar.isSchrikkeljaar());
     }
 
     @Test
     public void eenJaarDeelbaarDoor100IsGeenSchrikkeljaar() {
-        assertEquals(false, new Jaar(1900).isSchrikkeljaar());
+        assertFalse(new Jaar(1900).isSchrikkeljaar());
     }
 
     @Test
     public void eenJaarDeelbaarDoor4IsEenSchrikkeljaar() {
-        assertEquals(true, new Jaar(2012).isSchrikkeljaar());
+        assertTrue(new Jaar(2012).isSchrikkeljaar());
     }
 
     @Test
     public void eenJaarNietDeelbaarDoor4IsGeenSchrikkeljaar() {
-        assertEquals(false, new Jaar(2015).isSchrikkeljaar());
+        assertFalse(new Jaar(2015).isSchrikkeljaar());
     }
 }
